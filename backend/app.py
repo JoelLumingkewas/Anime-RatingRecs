9a17f9bd67abadd013efe151a1e29723956b646e
Joel <joelslumingkewas@gmail.com>
2025-07-17 15:09:48 -0400

    Made the frontend folders and the backend folders

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+# source venv/bin/activate
+# to go into virtual environment
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
